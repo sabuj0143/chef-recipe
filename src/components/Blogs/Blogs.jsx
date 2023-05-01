6871d29ac0536f65abbd06f56a9205a19fb4e0aa
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-01 22:50:36 +0600

    Blogs Complete

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -17,6 +17,11 @@ const Blogs = () => {
                 <h4 className='text-xl font-semibold'>3. Tell us the difference between nodejs and express js?</h4>
                 <p className='mt-2'>Ans : <small>NodeJS is the package, which provides the JavaScript run-time environment, whereas Express is a framework that sits on top of NodeJS and helps us to handle requests and responses.js web application framework that provides a robust set of features for web and mobile applications </small></p>
             </div>
+            <div className='mt-4'>
+                <h4 className='text-xl font-semibold'>4. What is a custom hook, and why will you create a custom hook?</h4>
+                <p className='mt-2'>Ans : <small>The fetch logic may be needed in other components as well, so we will extract that into a custom Hook. Custom Hooks are a mechanism to reuse stateful logic "such as setting up a subscription and remembering the current value", but every time you use a custom Hook, all state and effects inside of it are fully isolated. </small></p>
+            </div>
+            <hr  className='my-6'/>
         </div>
     );
 };

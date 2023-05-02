d0dae7aa6e223b1a395e5bf8727e4ea86484f0ea
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-02 19:48:55 +0600

    Footer added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
 
 const Main = () => {
     return (
         <div className='container w-full mx-auto'>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
